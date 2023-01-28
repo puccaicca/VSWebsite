7773d9360a55ab6dd31db57a54d4dcd1fa3a6b98
heart <heart@DESKTOP-A8EE423>
2023-01-27 17:33:17 -0800

    Add project files.

---
+++ VSWebsite/wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+function newColor() {
+    document.getElementById("Music").style.color = "orange";
+}
+
+function oldColor() {
+    document.getElementById("Music").style.color = "brown";
+}
